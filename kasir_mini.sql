c4b1f85da69639673a79ac993281430ba40b921b
michelaaaaaaa <michelahutama2409@gmail.com>
2025-11-23 22:19:09 +0700

    first commit

---
+++ kasir_mini.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE kasir_mini;
+USE kasir_mini;
+
+CREATE TABLE users (
+    id_user INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    nama VARCHAR(100) NOT NULL,
+    role ENUM('admin', 'kasir') NOT NULL
+);
+
+CREATE TABLE barang (
+    id_barang INT AUTO_INCREMENT PRIMARY KEY,
+    nama_barang VARCHAR(100) NOT NULL,
+    harga DOUBLE NOT NULL,
+    stok INT NOT NULL
+);
+
+CREATE TABLE transaksi (
+    id_transaksi INT AUTO_INCREMENT PRIMARY KEY,
+    tanggal DATETIME NOT NULL,
+    total DOUBLE NOT NULL,
+    id_user INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES user(id_user)
+);
+
+CREATE TABLE detail_transaksi (
+    id_detail INT AUTO_INCREMENT PRIMARY KEY,
+    id_transaksi INT NOT NULL,
+    id_barang INT NOT NULL,
+    qty INT NOT NULL,
+    harga_satuan DOUBLE NOT NULL,
+    subtotal DOUBLE NOT NULL,
+    FOREIGN KEY (id_transaksi) REFERENCES transaksi(id_transaksi),
+    FOREIGN KEY (id_barang) REFERENCES barang(id_barang)
+);
