15275c3e316c983e96899cf37a7b5ce202d59a4e
irmanurhayati <irman4970@gmail.com>
2025-12-09 12:10:19 +0700

    Update PrimaryController.java

--- kasirmini/src/main/java/com/kasirmini/kasirmini/PrimaryController.java
+++ kasirmini/src/main/java/com/kasirmini/kasirmini/PrimaryController.java
@@ -2,11 +2,39 @@ package com.kasirmini.kasirmini;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Label labelWelcome;
+
+    @FXML
+    private Button btnBarang;
+
+    @FXML
+    public void initialize() {
+        labelWelcome.setText("Selamat Datang di KasirMini!");
+    }
+
+    @FXML
+    private void goBarang() throws IOException {
+        App.setRoot("barang");
+    }
+
+    @FXML
+    private void goTransaksi() {
+        System.out.println("Menu Transaksi dipilih");
+    }
+
+    @FXML
+    private void goLaporan() {
+        System.out.println("Menu Laporan dipilih");
+    }
+
+    @FXML
+    private void logout() throws IOException {
+        App.setRoot("login");
     }
 }
